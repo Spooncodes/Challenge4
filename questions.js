efd1f7cd5690073de37950e8690957d79d1be0fe
Spooncodes <spoon.is.god@gmail.com>
2021-11-12 10:25:35 -0500

    fix bug

--- questions.js
+++ questions.js
@@ -192,7 +192,7 @@ function allDone() {
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
             // Travels to final page
-            window.location.replace("./HighScores.html");
+            window.location.replace("./highscores.html");
         }
     });
 
